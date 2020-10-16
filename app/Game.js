bace5a29c2b8fb32c8f4decf24f2ad7b3e1f1078
Wojciech Kaliszewski <D534549@euc.ppg.com>
2020-10-16 17:59:48 +0200

    Add styles, image and class

---
+++ app/Game.js
@@ -0,0 +1,52 @@
+import { Quote } from './Quote.js';
+
+class Game {
+  quotes = [
+    {
+      text: "pan tadeusz",
+      category: "Utwór literacki",
+    },
+    {
+      text: "janko muzykant",
+      category: "Utwór literacki",
+    },
+    {
+      text: "akademia pana kleksa",
+      category: "Film",
+    },
+    {
+      text: "ogniem i mieczem",
+      category: "Film",
+    },
+  ];
+
+  constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
+    this.lettersWrapper = lettersWrapper;
+    this.categoryWrapper = categoryWrapper;
+    this.wordWrapper = wordWrapper;
+    this.outputWrapper = outputWrapper;
+    const { text, category } = this.quotes[
+      Math.floor(Math.random() * this.quotes.length)
+    ];
+    this.categoryWrapper.innerHTML = category;
+    this.quote = new Quote(text);
+  }
+
+  guess(letter) {
+    console.log(letter);
+  }
+
+  drawLetters() {
+    for (let i = 0; i < 26; i++) {
+      const label = (i + 10).toString(36);
+      const button = document.createElement("button");
+      button.innerHTML = label;
+      button.addEventListener("click", () => this.guess(label));
+      this.lettersWrapper.appendChild(button);
+    }
+  }
+
+  start() {
+    this.drawLetters();
+  }
+}
---
+++ app/Quote.js
@@ -0,0 +1,5 @@
+export class Quote {
+  constructor(text) {
+    this.text = text;
+  }
+}
